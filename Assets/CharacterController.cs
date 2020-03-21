db218294603208286950f113df5aa88fc4a85c15
Evan McConnell <mcconnell.evan@gmail.com>
2020-03-21 01:47:15 +0000

    I need to push more consistently

---
+++ Assets/CharacterController.cs
@@ -0,0 +1,223 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+
+    [Header("Horizontal Movement")]
+    public float moveSpeed = 10f;
+    public Vector2 direction;
+    private bool facingRight = true;
+
+    [Header("Vertical Movement")]
+    public float jumpSpeed = 15f;
+    public float jumpDelay = 0.25f;
+    private float jumpTimer;
+
+    [Header("Components")]
+    public Rigidbody2D rb;
+    public Animator animator;
+    public LayerMask groundLayer;
+
+    [Header("Physics")]
+    public float maxSpeed = 4f;
+    public float linearDrag = 4f;
+    public float gravity = 1;
+    public float fallMultiplier = 5;
+    public bool changingDirections;
+
+    [Header("Launcher")]
+    public SpriteRenderer launchCursorSprite;
+    public float launchForce = 100f;
+    public float flickWindow = 0.5f;
+    private Vector2 flickDir;
+    private Vector2 finalFlickDir;
+    private float flickTime = 0f;
+    private bool flickable = false;
+    
+
+    [Header ("Collision")]
+    public bool onGround = false;
+    public float groundLength = 0.3f;
+
+    [Header("Velocity")]
+    public float xvelocity;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        onGround = Physics2D.Raycast(transform.position, Vector2.down, groundLength, groundLayer);
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            jumpTimer = Time.time + jumpDelay;
+        }
+
+        direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    void FixedUpdate()
+    {
+        MoveCharacter(direction.x);
+        if(jumpTimer > Time.time && onGround)
+        {
+            Jump();
+        }
+
+        modifyPhysics();
+
+        launchCursor();
+    }
+
+    void MoveCharacter(float horizontal)
+    {
+        rb.AddForce(Vector2.right * horizontal * moveSpeed);
+
+        animator.SetFloat("Horizontal", Mathf.Abs(rb.velocity.x));
+
+        if ((horizontal > 0 && !facingRight) || (horizontal < 0 && facingRight))
+        {
+            Flip();
+        }
+
+        if(Mathf.Abs(rb.velocity.x) > maxSpeed)
+        {
+            rb.velocity = new Vector2(Mathf.Sign(rb.velocity.x) * maxSpeed, rb.velocity.y);
+        }
+    }
+
+    void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, 0);
+        rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+        jumpTimer = 0;
+    }
+
+
+    /*
+    void modifyTime()
+    {
+        if (Input.GetKey(KeyCode.JoystickButton4))
+        {
+            Time.timeScale = 0.2f;
+            Time.fixedDeltaTime = 0.2f * 0.02f;
+        } else
+        {
+            Time.timeScale = 1;
+            Time.fixedDeltaTime = 0.02f;
+        }
+    }*/
+
+    void modifyPhysics()
+    {
+        changingDirections = (direction.x > 0 && rb.velocity.x < 0) || (direction.x < 0 && rb.velocity.x > 0);
+
+        animator.SetFloat("Vertical", rb.velocity.y);
+
+        if (onGround)
+        {
+            if (Mathf.Abs(direction.x) < 0.4f || changingDirections && onGround)
+            {
+                rb.drag = linearDrag;
+            }
+            else
+            {
+                rb.drag = 0;
+            }
+            rb.gravityScale = 0;
+        }
+        else
+        {
+            rb.gravityScale = gravity;
+            rb.drag = linearDrag * 0.15f;
+
+            if (rb.velocity.y < 0)
+            {
+                rb.gravityScale = gravity * fallMultiplier;
+            }
+            else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
+            {
+                rb.gravityScale = gravity * (fallMultiplier / 2);
+            }
+        } 
+    }
+
+    void launchCursor()
+    {
+        flickDir = new Vector2(Input.GetAxis("R_Horizontal"), Input.GetAxis("R_Vertical"));
+
+        if (flickDir.magnitude > 0.8)
+        {
+            finalFlickDir = new Vector2(Input.GetAxis("R_Horizontal"), Input.GetAxis("R_Vertical"));
+
+            launchCursorSprite.enabled = true;
+            Quaternion cursorDir = Quaternion.LookRotation((Vector3.right * flickDir.x + Vector3.up * flickDir.y), Vector3.back);
+            launchCursorSprite.gameObject.GetComponent<Rigidbody2D>().SetRotation(cursorDir);
+            launchCursorSprite.gameObject.transform.position = transform.position;
+
+            flickTime = Time.time;
+
+            //slow time
+            Time.timeScale = 0.2f;
+            Time.fixedDeltaTime = 0.2f * 0.02f;
+
+            flickable = true;
+
+        } else if(flickDir.magnitude >= 0.2)
+        {
+            flickable = true;
+
+            Time.timeScale = 1;
+            Time.fixedDeltaTime = 0.02f;
+
+            launchCursorSprite.enabled = false;
+        }
+        else if (flickDir.magnitude < 0.2 && (Time.time - flickWindow) < flickTime && flickable == true)
+        {
+            rb.velocity = new Vector2(0, 0);
+            launch(finalFlickDir);
+            print("flicked");
+
+            Time.timeScale = 1;
+            Time.fixedDeltaTime = 0.02f;
+
+            launchCursorSprite.enabled = false;
+
+            flickable = false;
+        }
+        else
+        {
+            launchCursorSprite.enabled = false;
+
+            //resume time
+            Time.timeScale = 1;
+            Time.fixedDeltaTime = 0.02f;
+        }
+    }
+
+    void launch(Vector2 launchDir)
+    {
+        rb.AddForce(launchDir*launchForce, ForceMode2D.Impulse);
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        transform.rotation = Quaternion.Euler(0, facingRight ? 0 : 180, 0);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * groundLength);
+        Gizmos.DrawLine(transform.position, transform.position + (Vector3.right * Input.GetAxis("R_Horizontal") + Vector3.up*Input.GetAxis("R_Vertical")));
+    }
+}
